7229bc66282482690c32c682014f608405250c6c
Diego532 <diegobastardo1@gmail.com>
2022-08-04 06:14:45 -0400

    Listo detalle de paciente con historia medica

--- src/lib/pages/Detalle_Paciente/detalle_paciente.dart
+++ src/lib/pages/Detalle_Paciente/detalle_paciente.dart
@@ -15,171 +15,176 @@ class DetallePaciente extends StatelessWidget {
   Widget build(BuildContext context) {
     final _width = ScreenSize.screenSize(context);
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.bottomLeft,
-        end: Alignment.topRight,
-        colors: [
-          AppColors.MAINCOLOR1,
-          AppColors.MAINCOLOR2,
-        ],
-      )),
-      child: Expanded(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          colors: [
+            AppColors.MAINCOLOR1,
+            AppColors.MAINCOLOR2,
+          ],
+        )),
+        child: SingleChildScrollView(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Divider(height: 30, color: Colors.transparent),
-          CustomText(
-            text:
-                "${paciente.primerNombre} ${paciente.segundoNombre} ${paciente.primerApellido} ${paciente.segundoApellido}",
-            size: 30,
-            weight: FontWeight.bold,
-            color: Colors.white,
-          ),
-          const Divider(height: 30, color: Colors.transparent),
-          Container(
-            width: 900,
-            height: 40,
-            child: const CustomText(
-              text: "Datos Personales: ",
-              size: 25,
-              weight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          const Divider(height: 10, color: Colors.transparent),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Correo: ${paciente.correo}",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Telefono: ${paciente.numeroMovil}",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Divider(height: 30, color: Colors.transparent),
+              CustomText(
                 text:
-                    "Fecha Nacimiento:: ${DateTime.parse(paciente.fechaNacimiento)}",
-                size: 20,
-                weight: FontWeight.normal,
+                    "${paciente.primerNombre} ${paciente.segundoNombre} ${paciente.primerApellido} ${paciente.segundoApellido}",
+                size: 30,
+                weight: FontWeight.bold,
                 color: Colors.white,
-              )),
-          const Divider(height: 30, color: Colors.transparent),
-          Container(
-            width: 900,
-            height: 40,
-            child: const CustomText(
-              text: "Medidas Del Paciente: ",
-              size: 25,
-              weight: FontWeight.bold,
-              color: Colors.white,
-            ),
+              ),
+              const Divider(height: 30, color: Colors.transparent),
+              Container(
+                width: 900,
+                height: 40,
+                child: const CustomText(
+                  text: "Datos Personales: ",
+                  size: 25,
+                  weight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const Divider(height: 10, color: Colors.transparent),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Correo: ${paciente.correo}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Telefono: ${paciente.numeroMovil}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text:
+                        "Fecha Nacimiento:: ${DateTime.parse(paciente.fechaNacimiento)}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              const Divider(height: 30, color: Colors.transparent),
+              Container(
+                width: 900,
+                height: 40,
+                child: const CustomText(
+                  text: "Medidas Del Paciente: ",
+                  size: 25,
+                  weight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const Divider(height: 10, color: Colors.transparent),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Peso: ${paciente.peso} kg",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Altura: ${paciente.altura} cm",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Genero: ${paciente.genero}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              const Divider(height: 30, color: Colors.transparent),
+              Container(
+                width: 900,
+                height: 40,
+                child: const CustomText(
+                  text: "Informacion Medica: ",
+                  size: 25,
+                  weight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Antecedentes: ${paciente.antecendentes}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 30,
+                  child: CustomText(
+                    text: "Operacion: ${paciente.operacion}",
+                    size: 20,
+                    weight: FontWeight.normal,
+                    color: Colors.white,
+                  )),
+              Container(
+                  width: 900,
+                  height: 40,
+                  child: const Center(
+                    child: CustomText(
+                      text: "Registros de Historias Medicas",
+                      size: 25,
+                      weight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )),
+              FutureBuilder(
+                  future: HistoriaMedica.fetchHistoriasMedicas(
+                      paciente), // historia medica
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<HistoriaMedica>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else {
+                      print(snapshot.data);
+                      print("cree el objeto");
+                      List<HistoriaMedica> snap = snapshot.data ?? [];
+                      return listHistoriaMedicaCard(context, snap, _width);
+                    }
+                  })
+            ],
           ),
-          const Divider(height: 10, color: Colors.transparent),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Peso: ${paciente.peso} kg",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Altura: ${paciente.altura} cm",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Genero: ${paciente.genero}",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          const Divider(height: 30, color: Colors.transparent),
-          Container(
-            width: 900,
-            height: 40,
-            child: const CustomText(
-              text: "Informacion Medica: ",
-              size: 25,
-              weight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Antecedentes: ${paciente.antecendentes}",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Container(
-              width: 900,
-              height: 30,
-              child: CustomText(
-                text: "Operacion: ${paciente.operacion}",
-                size: 20,
-                weight: FontWeight.normal,
-                color: Colors.white,
-              )),
-          Expanded(
-            child: Row(
-              children: [
-                FutureBuilder(
-                    future: HistoriaMedica.fetchHistoriasMedicas(
-                        paciente), // historia medica
-                    builder: (BuildContext context,
-                        AsyncSnapshot<List<HistoriaMedica>> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else {
-                        print(snapshot.data);
-                        print("cree el objeto");
-                        List<HistoriaMedica> snap = snapshot.data ?? [];
-                        return listHistoriaMedicaCard(context, snap, _width);
-                      }
-                    })
-              ],
-            ),
-          )
-        ],
-      )),
-    );
+        ));
   }
 }
 
 Widget listHistoriaMedicaCard(
     BuildContext context, List<HistoriaMedica> historiaMedica, double width) {
   return ListView.builder(
-      scrollDirection: Axis.horizontal,
+      scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemCount: historiaMedica.length,
       itemBuilder: ((context, index) {
         return Container(
-            padding:
-                const EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 30),
+            padding: const EdgeInsets.only(
+                left: 250, right: 250, top: 30, bottom: 30),
             child: HistoriaMedicaCard(
               historiaMedica: historiaMedica[index],
             ));
--- src/lib/widgets/historia_medica_card.dart
+++ src/lib/widgets/historia_medica_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myonlinedoctorweb/Modules/historia_medica.dart';
+import 'package:myonlinedoctorweb/constants/style.dart';
+import 'package:myonlinedoctorweb/widgets/custom_text.dart';
 
 class HistoriaMedicaCard extends StatelessWidget {
   HistoriaMedica historiaMedica;
@@ -9,6 +11,79 @@ class HistoriaMedicaCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 300,
+      width: 50,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(15)),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                ),
+                width: 800,
+                height: 100,
+                child: Center(
+                  child: CustomText(
+                    text: "Diagnostico: ${historiaMedica.diagnostico}",
+                    size: 20,
+                    weight: FontWeight.bold,
+                    color: active,
+                  ),
+                )),
+            Container(
+                padding: const EdgeInsets.only(top: 20),
+                width: 800,
+                height: 100,
+                child: Center(
+                  child: CustomText(
+                    text: "Plan: ${historiaMedica.plan}",
+                    size: 20,
+                    weight: FontWeight.bold,
+                    color: active,
+                  ),
+                )),
+            Container(
+                padding: const EdgeInsets.only(top: 20),
+                width: 800,
+                height: 100,
+                child: Center(
+                  child: CustomText(
+                    text: "Historia: ${historiaMedica.historia}",
+                    size: 20,
+                    weight: FontWeight.bold,
+                    color: active,
+                  ),
+                )),
+            Container(
+                padding: const EdgeInsets.only(top: 20),
+                width: 800,
+                height: 100,
+                child: Center(
+                  child: CustomText(
+                    text: "Examenes: ${historiaMedica.examenes}",
+                    size: 20,
+                    weight: FontWeight.bold,
+                    color: active,
+                  ),
+                )),
+            Container(
+                padding: const EdgeInsets.only(top: 20),
+                width: 800,
+                height: 100,
+                child: Center(
+                  child: CustomText(
+                    text: "Prescripcion: ${historiaMedica.prescripcion}",
+                    size: 20,
+                    weight: FontWeight.bold,
+                    color: active,
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
